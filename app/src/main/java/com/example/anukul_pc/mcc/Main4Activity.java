538e05bc8b3946e53cb0df99c4426c9ff69a44ba
DHINGLI DHINGLO <solankimimoh@gmail.com>
2018-05-16 13:38:13 +0530

    merge

--- app/src/main/java/com/example/anukul_pc/mcc/Main4Activity.java
+++ app/src/main/java/com/example/anukul_pc/mcc/Main4Activity.java
@@ -2,9 +2,6 @@ package com.example.anukul_pc.mcc;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -81,7 +78,7 @@ public class Main4Activity extends AppCompatActivity
 
         } else if (id == R.id.SelectVM) {
 
-            final Intent intent = new Intent(Main4Activity.this,selectVMActivity.class);
+            final Intent intent = new Intent(Main4Activity.this,SelectVMActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.Tools) {
--- app/src/main/java/com/example/anukul_pc/mcc/SelectVMActivity.java
+++ app/src/main/java/com/example/anukul_pc/mcc/SelectVMActivity.java
@@ -3,7 +3,7 @@ package com.example.anukul_pc.mcc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class selectVMActivity extends AppCompatActivity {
+public class SelectVMActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
